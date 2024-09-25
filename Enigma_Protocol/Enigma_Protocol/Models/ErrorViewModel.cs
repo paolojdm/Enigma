5f9dfbee8984d17abe5ce9a4971faed3a5f7c13d
SuprJwJ <vovaprystupa42@gmail.com>
2024-09-25 12:31:47 +0200

    Created Enigam_Protocol project sln

---
+++ Enigma_Protocol/Enigma_Protocol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Enigma_Protocol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
