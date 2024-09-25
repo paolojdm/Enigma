ce87f8865530419a6c3bb61b72635b57b1a4ee21
SuprJwJ <vovaprystupa42@gmail.com>
2024-09-25 12:42:18 +0200

    Created LoginController and view Login.cshtml

---
+++ Enigma_Protocol/Enigma_Protocol/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enigma_Protocol.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+            //Commento
+        }
+
+
+    }
+}
