3c166b850053617a1704fcff2575cfee86444363
SuprJwJ <vovaprystupa42@gmail.com>
2024-09-26 15:21:55 +0200

    AddedPuzzleControlV1

---
+++ Enigma_Protocol/Enigma_Protocol/Controllers/PuzzleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enigma_Protocol.Controllers
+{
+    public class PuzzleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Puzzle()
+        {
+            return View();
+        }
+    }
+}
