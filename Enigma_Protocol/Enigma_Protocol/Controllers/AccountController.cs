c9f30bc97645e6386937c0ad4704fab8a36b769f
SuprJwJ <vovaprystupa42@gmail.com>
2024-09-26 13:33:10 +0200

    Fixed AccountController and relative Views

--- Enigma_Protocol/Enigma_Protocol/Controllers/AccountController.cs
+++ Enigma_Protocol/Enigma_Protocol/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Enigma_Protocol.Models;
 using System.Security.Claims;
 using System;
 using Enigma_Protocol.DB;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Enigma_Protocol.Controllers
 {
@@ -19,8 +20,6 @@ namespace Enigma_Protocol.Controllers
             _context = context;
             _passwordHasher = new PasswordHasher<User>();
         }
-
-
 
         #region LOgin
         [HttpGet]
@@ -75,19 +74,6 @@ namespace Enigma_Protocol.Controllers
 
         #endregion Login
 
-       
-
-        //public async Task<IActionResult> Register(User user, string password)//deprecated
-        //{
-        //    var passwordHasher = new PasswordHasher<User>();
-        //    user.PasswordHash = passwordHasher.HashPassword(user, password);
-
-        //    _context.Users.Add(user);
-        //    await _context.SaveChangesAsync();
-
-        //    return RedirectToAction("Login", "Account");
-        //}
-
         #region Register
 
         [HttpGet]
@@ -140,5 +126,41 @@ namespace Enigma_Protocol.Controllers
         }
 
         #endregion Register
+
+        #region MyAccount
+
+        [Authorize] // Ensure only logged-in users can access
+        public async Task<IActionResult> MyAccount()
+        {
+            // Get the logged-in user
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _context.Users
+                                     .Include(u => u.playerProgresses) // Assuming PlayerProgress is a related entity
+                                     .FirstOrDefaultAsync(u => u.Id == int.Parse(userId));
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            // Fetch the PlayerProfile by matching PlayerName to UserName
+            var playerProgress = await _context.PlayerProgresses
+                                              .FirstOrDefaultAsync(p => p.PlayerID == user.Id);
+
+            // Fetch solved puzzles from PlayerProgress (example statistic)
+            var solvedPuzzlesCount = playerProgress.SolvedPuzzles;
+
+            var model = new MyAccountViewModel
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                ShippingAddress = user.ShippingAddress,
+                CreatedAt = user.CreatedAt,
+                SolvedPuzzlesCount = solvedPuzzlesCount
+            };
+
+            return View(model);
+        }
+
+        #endregion MyAccount
     }//end public class
 }
