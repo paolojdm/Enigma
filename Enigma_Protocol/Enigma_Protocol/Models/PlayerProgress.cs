cd6bdb885e6b1d6130c7881dbf4c83c9b5625f18
SuprJwJ <vovaprystupa42@gmail.com>
2024-10-02 15:22:56 +0200

    Updated PlayerProgress

--- Enigma_Protocol/Enigma_Protocol/Models/PlayerProgress.cs
+++ Enigma_Protocol/Enigma_Protocol/Models/PlayerProgress.cs
@@ -6,6 +6,11 @@
         public int CurrentRoomId { get; set; }
         public int SolvedPuzzles { get; set; }
         public int PlayerID { get; set; }
+        public int Current_Lives_Room { get; set; } //Quante vite ha nella stanza (e' quante vite sono rimaste)
+        public int Current_Lives_Puzzle { get; set; } //QUanti tentativi ha rimasti per completare il puzzle (e' quante vite sono rimaste)
+
+        public DateTime RoomStartTime { get; set; } //Tempo preso dal momento in cui entra nella stanza
+        public DateTime CurrentRoomTime { get; set; } //Si aggiorna ogni secondo, se la differenza tra il RoomStartTime e il CurrentRooomTime supera il tempo indicato il gioco finisce (gestitevela voi)
 
         // Navigation Properties
         public Room CurrentRoom { get; set; }
