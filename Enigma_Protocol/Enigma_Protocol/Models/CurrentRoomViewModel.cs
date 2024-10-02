acd24c0dc80fdf4d6cc328359815cfdf455535fc
SuprJwJ <vovaprystupa42@gmail.com>
2024-10-02 15:24:29 +0200

    Added CurrentRoomViewModel

---
+++ Enigma_Protocol/Enigma_Protocol/Models/CurrentRoomViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Enigma_Protocol.Models
+{
+    public class CurrentRoomViewModel
+    {
+        public PlayerProgress PlayerProgress { get; set; }
+        public Room CurrentRoom { get; set; }
+        public Puzzle CurrentPuzzle { get; set; }
+    }
+}
